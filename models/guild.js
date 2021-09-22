c49e294b95970d66045082265d49ef3195dc3c6a
QuaBot <quabotgithub@gmail.com>
2021-09-22 18:59:33 +0200

    Added QuaBot
    Imported all files to the Github Respatory

---
+++ models/guild.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const guildSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    guildID: String,
+    guildName: String,
+    prefix: String,
+    logChannelID: String,
+    enableLog: String,
+    enableSwearFilter: String,
+    enableMusic: String,
+    enableLevel: String,
+    mutedRoleName: String,
+    mainRoleName: String
+});
+
+module.exports = mongoose.model('Guild', guildSchema, 'guilds');
