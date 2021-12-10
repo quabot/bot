420983f212dc5e3b209cab80513658287b7c8631
QuaBot <85528051+QuaBot@users.noreply.github.com>
2021-12-10 15:51:04 +0100

    sdsdsdsdsdsdsdsdsdsd

---
+++ events/guild/channelPinsUpdate.js
@@ -0,0 +1,67 @@
+const { commands } = require('../../index');
+const { Client, MessageEmbed } = require('discord.js');
+const colors = require('../../files/colors.json');
+const consola = require('consola');
+const mongoose = require('mongoose');
+
+module.exports = {
+    name: "channelPinsUpdate",
+    /**
+     * @param {Client} client 
+     */
+    async execute(channel, time) {
+
+        const Guild = require('../../schemas/GuildSchema');
+        const guildDatabase = await Guild.findOne({
+            guildId: channel.guild.id,
+        }, (err, guild) => {
+            if (err) console.error(err);
+            if (!guild) {
+                const newGuild = new Guild({
+                    guildId: channel.guild.id,
+                    guildName: channel.guild.name,
+                    logChannelID: "none",
+                    reportChannelID: "none",
+                    suggestChannelID: "none",
+                    welcomeChannelID: "none",
+                    levelChannelID: "none",
+                    pollChannelID: "none",
+                    ticketCategory: "Tickets",
+                    closedTicketCategory: "Tickets",
+                    logEnabled: true,
+                    musicEnabled: true,
+                    levelEnabled: true,
+                    reportEnabled: true,
+                    suggestEnabled: true,
+                    ticketEnabled: true,
+                    welcomeEnabled: true,
+                    pollsEnabled: true,
+                    roleEnabled: true,
+                    mainRole: "Member",
+                    mutedRole: "Muted"
+                });
+                newGuild.save()
+                    .catch(err => {
+                        console.log(err);
+                    });
+                return;
+            }
+        });
+
+        const logChannel = channel.guild.channels.cache.get(guildDatabase.logChannelID);
+
+
+        if (guildDatabase.logEnabled === "true") {
+            if (logChannel) {
+                const embed = new MessageEmbed()
+                    .setColor(colors.COLOR)
+                    .setTitle('Channel pins were updated!')
+                    .addField('Channel', `${channel}`)
+                    .addField('Channel-ID', `${channel.id}`)
+                    .addField("Time", `${time}`)
+                    .setTimestamp()
+                logChannel.send({ embeds: [embed] });
+            };
+        }
+    }
+}
