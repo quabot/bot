07e36a4995c72ec5a7baccff2b22a7f0ed7f9f3c
Joa_sss <quabotgithub@gmail.com>
2022-01-30 23:45:40 +0100

    Shop Page 1

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const colors = require('../../files/colors.json');
 
 const { errorMain } = require('../../files/embeds');
+const shop = require('../../validation/shop.json');
 
 module.exports = {
     name: "shop",
@@ -31,10 +32,71 @@ module.exports = {
                             console.log(err);
                             interaction.channel.send({ embeds: [errorMain] });
                         });
-                        const addedEmbed = new discord.MessageEmbed().setColor(colors.COLOR).setDescription("You can use economy commands now.")
-                        return interaction.channel.send({ embeds: [addedEmbed] });
+                    const addedEmbed = new discord.MessageEmbed().setColor(colors.COLOR).setDescription("You can use economy commands now.")
+                    return interaction.channel.send({ embeds: [addedEmbed] });
                 }
             });
+
+            const shopPage1Select = new discord.MessageActionRow()
+                .addComponents(
+                    new discord.MessageSelectMenu()
+                        .setCustomId('select')
+                        .setPlaceholder('Select a product to buy.')
+                        .setMinValues(1)
+                        .setMaxValues(1)
+                        .addOptions([
+                            {
+                                label: `ICON HERE ${shop[0].item}`,
+                                description: `${shop[0].description}`,
+                                value: 'shop0',
+                            },
+                            {
+                                label: `${shop[1].item}`,
+                                description: `${shop[1].description}`,
+                                value: 'shop1',
+                            },
+                            {
+                                label: `${shop[2].item}`,
+                                description: `${shop[2].description}`,
+                                value: 'shop2',
+                            },
+                            {
+                                label: `${shop[3].item}`,
+                                description: `${shop[3].description}`,
+                                value: 'shop3',
+                            },
+                            {
+                                label: `${shop[4].item}`,
+                                description: `${shop[4].description}`,
+                                value: 'shop4',
+                            },
+                        ]),
+                );
+
+            const shopPage1Buttons = new discord.MessageActionRow()
+                .addComponents(
+                    new discord.MessageButton()
+                        .setCustomId('page1dis')
+                        .setDisabled(true)
+                        .setLabel('🡰')
+                        .setStyle('PRIMARY'),
+                    new discord.MessageButton()
+                        .setCustomId('page1next')
+                        .setLabel('🡲')
+                        .setStyle('PRIMARY'),
+                );
+
+            const shopPage1Embed = new discord.MessageEmbed()
+                .setTitle("Shop items - 1/2")
+                .setDescription("Use the selection menu to purchase items.")
+                .addField(`${shop[0].emoji} ${shop[0].item} - \`⑩ ${shop[0].prize}\``, `${shop[0].description}`)
+                .addField(`${shop[1].emoji} ${shop[1].item} - \`⑩ ${shop[1].prize}\``, `${shop[1].description}`)
+                .addField(`${shop[2].emoji} ${shop[2].item} - \`⑩ ${shop[2].prize}\``, `${shop[2].description}`)
+                .addField(`${shop[3].emoji} ${shop[3].item} - \`⑩ ${shop[3].prize}\``, `${shop[3].description}`)
+                .addField(`${shop[4].emoji} ${shop[4].item} - \`⑩ ${shop[4].prize}\``, `${shop[4].description}`)
+                .setTimestamp()
+                .setColor(colors.COLOR)
+            interaction.reply({ embeds: [shopPage1Embed], components: [shopPage1Select, shopPage1Buttons] });
         } catch (e) {
             console.log(e);
             return;
