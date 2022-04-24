1edaf4e54237241be0fd898652857e547186a98a
Joa_sss <quabotgithub@gmail.com>
2022-04-24 14:07:48 +0200

    EmojiCreate event

--- events/logging/emojiCreate.js
+++ events/logging/emojiCreate.js
@@ -1,10 +1,56 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Message } = require('discord.js');
 
 module.exports = {
     name: "emojiCreate",
-    execute(args, client, color) {
+    async execute(emoji, client, color) {
         try {
-             
+
+            const Guild = require('../../structures/schemas/GuildSchema');
+            const guildDatabase = await Guild.findOne({
+                guildId: emoji.guild.id,
+            }, (err, guild) => {
+                if (err) console.error(err);
+                if (!guild) {
+                    const newGuild = new Guild({
+                        guildId: emoji.guild.id,
+                        guildName: emoji.guild.name,
+                        logChannelID: "none",
+                        suggestChannelID: "none",
+                        welcomeChannelID: "none",
+                        levelChannelID: "none",
+                        logEnabled: true,
+                        levelEnabled: false,
+                        suggestEnabled: true,
+                        welcomeEnabled: true,
+                        roleEnabled: false,
+                        mainRole: "Member",
+                        joinMessage: "Welcome {user} to **{guild}**!",
+                        leaveMessage: "Goodbye {user}!",
+                        swearEnabled: false,
+                    });
+                    newGuild.save()
+                        .catch(err => {
+                            console.log(err);
+                            interaction.channel.send({ embeds: [new MessageEmbed().setDescription("There was an error with the database.").setColor(color)] }).catch(err => console.log(err));
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!guildDatabase) return;
+            if (guildDatabase.logEnabled === false) return;
+            const channel = emoji.guild.channels.cache.get(guildDatabase.logChannelID);
+            if (!channel) return;
+
+            channel.send({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(color)
+                        .setTitle("Emoji Created!")
+                        .addField('Emoji Name', `${emoji.name}`)
+                        .setFooter(`ID: ${emoji.id}`, `${emoji.url}`)
+                ]
+            });
+
         } catch (e) {
             console.log(e);
             client.guilds.cache.get("957024489638621185").channels.cache.get("957024594181644338").send({ embeds: [new MessageEmbed().setDescription(`${e}`).setFooter("Event: " + this.name)] });
