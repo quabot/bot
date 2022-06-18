35fb04ca663b809b334c4badb72223b299fe2fc4
Joa_sss <quabotgithub@gmail.com>
2022-06-18 20:51:45 +0200

    Channel Member Counts

---
+++ events/systems/channelMembersLeave.js
@@ -0,0 +1,90 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const canvacord = require('canvacord');
+
+module.exports = {
+    name: "guildMemberRemove",
+    async execute(member, client, color) {
+        try {
+            // DM & Bot checks
+            if (!member.guild) return;
+
+            // Find the guild's database
+            const Guild = require('../../structures/schemas/GuildSchema');
+            const guildDatabase = await Guild.findOne({
+                guildId: member.guild.id,
+            }, (err, guild) => {
+                if (err) console.error(err);
+                if (!guild) {
+                    const newGuild = new Guild({
+                        guildId: member.guild.id,
+                        guildName: member.guild.name,
+                        logChannelID: "none",
+                        ticketCategory: "none",
+                        ticketClosedCategory: "none",
+                        ticketEnabled: true,
+                        ticketStaffPing: true,
+                        ticketTopicButton: true,
+                        ticketSupport: "none",
+                        ticketId: 1,
+                        ticketLogs: true,
+                        ticketChannelID: "none",
+                        afkStatusAllowed: "true",
+                        musicEnabled: "true",
+                        musicOneChannelEnabled: "false",
+                        musicChannelID: "none",
+                        suggestChannelID: "none",
+                        logSuggestChannelID: "none",
+                        logPollChannelID: "none",
+                        afkEnabled: true,
+                        welcomeChannelID: "none",
+                        leaveChannelID: "none",
+                        levelChannelID: "none",
+                        punishmentChannelID: "none",
+                        pollID: 0,
+                        logEnabled: true,
+                        modEnabled: true,
+                        levelEnabled: false,
+                        welcomeEmbed: true,
+                        pollEnabled: true,
+                        suggestEnabled: true,
+                        welcomeEnabled: true,
+                        leaveEnabled: true,
+                        roleEnabled: false,
+                        mainRole: "none",
+                        joinMessage: "Welcome {user} to **{guild}**!",
+                        leaveMessage: "Goodbye {user}!",
+                        swearEnabled: false,
+                        levelCard: false,
+                        levelEmbed: true,
+                        levelMessage: "{user} just leveled up to level **{level}**!",
+                        membersChannel: "none",
+                        membersMessage: "Members: {count}",
+                        memberEnabled: true
+                    });
+                    newGuild.save()
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!guildDatabase) return;
+
+            if (guildDatabase.memberEnabled === false) return;
+
+            const channel = member.guild.channels.cache.get(`${guildDatabase.membersChannel}`);
+
+            if (!channel) return;
+            
+            let name = guildDatabase.membersMessage;
+            name = name.replaceAll("{count}", `${member.guild.memberCount}`);
+            name = name.replaceAll("{guild}", `${member.guild.name}`);
+
+            channel.setName(`${name}`).catch(( err => { } ));
+
+        } catch (e) {
+            console.log(e);
+            client.guilds.cache.get("957024489638621185").channels.cache.get("957024594181644338").send({ embeds: [new MessageEmbed().setDescription(`${e}`).setFooter("Event: " + this.name)] });
+        }
+    }
+}
