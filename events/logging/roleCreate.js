51da8313dbfdc4d4518c8ff92f30da4db568cd0f
Joa_sss <quabotgithub@gmail.com>
2022-06-21 22:04:06 +0200

    roleCreateDelete

---
+++ events/logging/roleCreate.js
@@ -0,0 +1,137 @@
+const { MessageEmbed, Message } = require('discord.js');
+
+module.exports = {
+    name: "roleCreate",
+    async execute(role, client, color) {
+        try {
+
+            const Guild = require('../../structures/schemas/GuildSchema');
+            const guildDatabase = await Guild.findOne({
+                guildId: role.guild.id,
+            }, (err, guild) => {
+                if (err) console.error(err);
+                if (!guild) {
+                    const newGuild = new Guild({
+                        guildId: role.guild.id,
+                        guildName: role.guild.name,
+                        logChannelID: "none",
+                        ticketCategory: "none",
+                        ticketClosedCategory: "none",
+                        ticketEnabled: true,
+                        ticketStaffPing: true,
+                        ticketTopicButton: true,
+                        ticketSupport: "none",
+                        ticketId: 1,
+                        ticketLogs: true,
+                        ticketChannelID: "none",
+                        afkStatusAllowed: "true",
+                        musicEnabled: "true",
+                        musicOneChannelEnabled: "false",
+                        musicChannelID: "none",
+                        suggestChannelID: "none",
+                        logSuggestChannelID: "none",
+                        logPollChannelID: "none",
+                        afkEnabled: true,
+                        welcomeChannelID: "none",
+                        leaveChannelID: "none",
+                        levelChannelID: "none",
+                        punishmentChannelID: "none",
+                        punishmentChannelID: "none",
+                        pollID: 0,
+                        logEnabled: true,
+                        modEnabled: true,
+                        levelEnabled: false,
+                        welcomeEmbed: true,
+                        pollEnabled: true,
+                        suggestEnabled: true,
+                        welcomeEnabled: true,
+                        leaveEnabled: true,
+                        roleEnabled: false,
+                        mainRole: "none",
+                        joinMessage: "Welcome {user} to **{guild}**!",
+                        leaveMessage: "Goodbye {user}!",
+                        swearEnabled: false,
+                        levelCard: false,
+                        levelEmbed: true,
+                        levelMessage: "{user} just leveled up to level **{level}**!",
+                        membersChannel: "none",
+                        membersMessage: "Members: {count}",
+                        memberEnabled: true
+                    });
+                    newGuild.save()
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!guildDatabase) return;
+            if (guildDatabase.logEnabled === false) return;
+
+            const channel = role.guild.channels.cache.get(guildDatabase.logChannelID);
+            if (!channel) return;
+            if (channel.type === "GUILD_VOICE") return;
+            if (channel.type === "GUILD_STAGE_VOICE") return;
+
+            const Log = require('../../structures/schemas/LogSchema');
+            const logDatabase = await Log.findOne({
+                guildId: role.guild.id,
+            }, (err, log) => {
+                if (err) console.error(err);
+                if (!log) {
+                    const newLog = new Log({
+                        guildId: role.guild.id,
+                        enabled: [
+                            'emojiCreateDelete',
+                            'emojiUpdate',
+                            'guildBanAdd',
+                            'guildBanRemove',
+                            'roleAddRemove',
+                            'nickChange',
+                            'channelCreateDelete',
+                            'channelUpdate',
+                            'inviteCreateDelete',
+                            'messageDelete',
+                            'messageUpdate',
+                            'roleCreateDelete',
+                            'roleUpdate',
+                            'stickerCreateDelete',
+                            'stickerUpdate',
+                            'threadCreateDelete',
+                        ],
+                        disabled: [
+                            'voiceMove',
+                            'voiceJoinLeave',
+                        ]
+                    });
+                    newLog.save()
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!logDatabase) return;
+
+            let description = `**Role Created**\n${role}`;
+            let perms = role.permissions.toArray().join("\n")
+            let permsLength = String(perms);
+            if (permsLength.length < 971) description = `${description}\n\n**Permissions:**\n\`${perms}\``
+
+            if (logDatabase.enabled.includes("roleCreateDelete")) {
+                channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor(color)
+                            .setDescription(`${description}`)
+                            .setTimestamp()
+                            .setFooter({ text: `Role Name: ${role.name}` })
+                    ]
+                }).catch((err => { }));
+            }
+        } catch (e) {
+            console.log(e);
+            client.guilds.cache.get("957024489638621185").channels.cache.get("957024594181644338").send({ embeds: [new MessageEmbed().setDescription(`${e}`).setFooter("Event: " + this.name)] });
+        }
+    }
+}
---
+++ events/logging/roleDelete.js
@@ -0,0 +1,135 @@
+const { MessageEmbed, Message } = require('discord.js');
+
+module.exports = {
+    name: "roleDelete",
+    async execute(role, client, color) {
+        try {
+
+            const Guild = require('../../structures/schemas/GuildSchema');
+            const guildDatabase = await Guild.findOne({
+                guildId: role.guild.id,
+            }, (err, guild) => {
+                if (err) console.error(err);
+                if (!guild) {
+                    const newGuild = new Guild({
+                        guildId: role.guild.id,
+                        guildName: role.guild.name,
+                        logChannelID: "none",
+                        ticketCategory: "none",
+                        ticketClosedCategory: "none",
+                        ticketEnabled: true,
+                        ticketStaffPing: true,
+                        ticketTopicButton: true,
+                        ticketSupport: "none",
+                        ticketId: 1,
+                        ticketLogs: true,
+                        ticketChannelID: "none",
+                        afkStatusAllowed: "true",
+                        musicEnabled: "true",
+                        musicOneChannelEnabled: "false",
+                        musicChannelID: "none",
+                        suggestChannelID: "none",
+                        logSuggestChannelID: "none",
+                        logPollChannelID: "none",
+                        afkEnabled: true,
+                        welcomeChannelID: "none",
+                        leaveChannelID: "none",
+                        levelChannelID: "none",
+                        punishmentChannelID: "none",
+                        punishmentChannelID: "none",
+                        pollID: 0,
+                        logEnabled: true,
+                        modEnabled: true,
+                        levelEnabled: false,
+                        welcomeEmbed: true,
+                        pollEnabled: true,
+                        suggestEnabled: true,
+                        welcomeEnabled: true,
+                        leaveEnabled: true,
+                        roleEnabled: false,
+                        mainRole: "none",
+                        joinMessage: "Welcome {user} to **{guild}**!",
+                        leaveMessage: "Goodbye {user}!",
+                        swearEnabled: false,
+                        levelCard: false,
+                        levelEmbed: true,
+                        levelMessage: "{user} just leveled up to level **{level}**!",
+                        membersChannel: "none",
+                        membersMessage: "Members: {count}",
+                        memberEnabled: true
+                    });
+                    newGuild.save()
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!guildDatabase) return;
+            if (guildDatabase.logEnabled === false) return;
+
+            const channel = role.guild.channels.cache.get(guildDatabase.logChannelID);
+            if (!channel) return;
+            if (channel.type === "GUILD_VOICE") return;
+            if (channel.type === "GUILD_STAGE_VOICE") return;
+
+            const Log = require('../../structures/schemas/LogSchema');
+            const logDatabase = await Log.findOne({
+                guildId: role.guild.id,
+            }, (err, log) => {
+                if (err) console.error(err);
+                if (!log) {
+                    const newLog = new Log({
+                        guildId: role.guild.id,
+                        enabled: [
+                            'emojiCreateDelete',
+                            'emojiUpdate',
+                            'guildBanAdd',
+                            'guildBanRemove',
+                            'roleAddRemove',
+                            'nickChange',
+                            'channelCreateDelete',
+                            'channelUpdate',
+                            'inviteCreateDelete',
+                            'messageDelete',
+                            'messageUpdate',
+                            'roleCreateDelete',
+                            'roleUpdate',
+                            'stickerCreateDelete',
+                            'stickerUpdate',
+                            'threadCreateDelete',
+                        ],
+                        disabled: [
+                            'voiceMove',
+                            'voiceJoinLeave',
+                        ]
+                    });
+                    newLog.save()
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+            }).clone().catch(function (err) { console.log(err) });
+
+            if (!logDatabase) return;
+
+            let description = `**Role Deleted**\n${role.name}`;
+            let perms = role.permissions.toArray().join("\n");
+
+            if (logDatabase.enabled.includes("roleCreateDelete")) {
+                channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor(`${role.hexColor}`)
+                            .setDescription(`${description}`)
+                            .setTimestamp()
+                            .setFooter({ text: `Role Name: ${role.name}` })
+                    ]
+                }).catch((err => { }));
+            }
+        } catch (e) {
+            console.log(e);
+            client.guilds.cache.get("957024489638621185").channels.cache.get("957024594181644338").send({ embeds: [new MessageEmbed().setDescription(`${e}`).setFooter("Event: " + this.name)] });
+        }
+    }
+}
