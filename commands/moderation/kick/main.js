781eb6bafff4b739306b68757edb47d285f64384
foxl <every.flipped@gmail.com>
2022-07-04 17:57:41 +0100

    made ban and kick

--- commands/moderation/kick/main.js
+++ commands/moderation/kick/main.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "ban",
-    description: 'Ban a user.',
-    permission: "BAN_MEMBERS",
+    name: "kick",
+    description: 'Kick a user.',
+    permission: "KICK_MEMBERS",
     options: [
         {
             name: "user",
-            description: "Who should QuaBot ban?",
+            description: "Who should QuaBot kick?",
             type: "USER",
             required: true,
         },
         {
             name: "reason",
-            description: "Why should that user be banned?",
+            description: "Why should that user be kicked?",
             type: "STRING",
             required: false,
         },
@@ -29,63 +29,63 @@ module.exports = {
         let reason = interaction.options.getString('reason') ? interaction.options.getString('reason') : "No reason given";
         let private = interaction.options.getBoolean('private') ? true : false;
         if (reason.length > 1000) reason = "No reason specified.";
-        let didBan = true;
+        let didKick = true;
 
         if (!member) {
-            didBan = false;
+            didKick = false;
             return interaction.reply({
                 embeds: [
                     new MessageEmbed()
                         .setTitle("<:error:990996645913194517> Unspecified argument")
-                        .setDescription(`Please specify a user to ban`)
+                        .setDescription(`Please specify a user to kick`)
                         .setColor(color)
                 ], ephemeral: private
             }).catch((err => { }))
         }
 
         if (member.id === interaction.member.id) {
-            didBan = false;
+            didKick = false;
             return interaction.reply({
                 embeds: [
                     new MessageEmbed()
                         .setTitle("<:error:990996645913194517> What is wrong with you?")
-                        .setDescription(`You can't ban yourself!`)
+                        .setDescription(`You can't kick yourself!`)
                         .setColor(color)
                 ], ephemeral: private
             }).catch((err => { }))
         }
 
         if (member.roles.highest.rawPosition > interaction.member.roles.highest.rawPosition) {
-            didBan = false;
+            didKick = false;
             return interaction.reply({
                 embeds: [
                     new MessageEmbed()
                         .setTitle("<:error:990996645913194517> Insufficcient permissions")
-                        .setDescription(`You cannot ban a user with roles higher than your own`)
+                        .setDescription(`You cannot kick a user with roles higher than your own`)
                         .setColor(color)
                 ], ephemeral: private
             }).catch((err => { }))
         }
 
         if (member.roles.highest.rawPosition > interaction.guild.members.cache.get(client.user.id).roles.highest.rawPosition) {
-            didBan = false;
+            didKick = false;
             return interaction.reply({
                 embeds: [
                     new MessageEmbed()
                         .setTitle("<:error:990996645913194517> Insufficcient permissions")
-                        .setDescription(`QuaBot does not have permission to ban that user - try moving the QuaBot role above all others`)
+                        .setDescription(`QuaBot does not have permission to kick that user - try moving the QuaBot role above all others`)
                         .setColor(color)
                 ], ephemeral: private
             }).catch((err => { }))
         }
 
-        if (didBan) {
+        if (didKick) {
             if (!private) {member.send({
                 embeds: [
                     new MessageEmbed()
-                        .setTitle(`<:error:990996645913194517> You were banned`)
-                        .setDescription(`You were banned from **${interaction.guild.name}**
-                    **Banned by**: ${interaction.user}
+                        .setTitle(`<:error:990996645913194517> You were kicked`)
+                        .setDescription(`You were kicked from **${interaction.guild.name}**
+                    **Kicked by**: ${interaction.user}
                     **Reason**: ${reason}`)
                         .setTimestamp()
                         .setColor(color)
@@ -94,11 +94,11 @@ module.exports = {
             interaction.reply({
                 embeds: [
                     new MessageEmbed()
-                        .setTitle(`<:online:938818583868366858> User banned`)
+                        .setTitle(`<:online:938818583868366858> User kicked`)
                         .setDescription(`**User**: ${member}`)
                         .setColor(color)
                         .addFields(
-                            { name: "Ban-ID", value: `Currently unavailable`, inline: true },
+                            { name: "Kick-ID", value: `Currently unavailable`, inline: true },
                             { name: "Reason", value: `${reason}`, inline: true },
                             { name: "\u200b", value: "\u200b", inline: true },
                             { name: "Joined Server", value: `<t:${parseInt(member.joinedTimestamp / 1000)}:R>`, inline: true },
@@ -109,15 +109,15 @@ module.exports = {
             }).catch((err => { }))
         }
 
-        member.ban({ reason: reason }).catch(err => {
-            didBan = false;
+        member.kick({ reason: reason }).catch(err => {
+            didKick = false;
             if (err.code === 50013) {
                 interaction.deleteReply();
                 return interaction.channel.send({
                     embeds: [
                         new MessageEmbed()
                             .setTitle("<:error:990996645913194517> Insufficcient permissions")
-                            .setDescription(`QuaBot does not have permission to ban that user - try moving the QuaBot role above all others`)
+                            .setDescription(`QuaBot does not have permission to kick that user - try moving the QuaBot role above all others`)
                             .setColor(color)
                     ], ephemeral: private
                 }).catch((err => { }))
