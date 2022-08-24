a8ba4c928ad7e3907bd809cad9fd09b55e158941
Joa_sss <quabotgithub@gmail.com>
2022-08-24 21:44:05 +0200

    feat: added voice move logging

---
+++ src/events/logging/voiceMove.js
@@ -0,0 +1,42 @@
+const { Client, EmbedBuilder, Colors, GuildBan, VoiceChannel } = require('discord.js');
+const { getLogConfig, getLogChannel } = require('../../structures/functions/config');
+
+module.exports = {
+    event: "voiceStateUpdate",
+    name: "voiceMove",
+    /**
+     * @param {VoiceChannel} oldState
+     * @param {VoiceChannel} newState
+     * @param {Client} client
+     */
+    async execute(oldState, newState, client, color) {
+
+        if (!newState.guild) return;
+
+        const logConfig = await getLogConfig(client, newState.guild.id);
+        if (!logConfig) return;
+
+        const logChannel = await getLogChannel(newState.guild, logConfig);
+        if (!logChannel) return;
+
+        if (!logConfig.enabledEvents.includes(this.name)) return;
+
+        if (oldState.channelId === newState.channelId) return;
+        if (!oldState.channelId || !newState.channelId) return;
+
+        const user = newState.guild.members.cache.get(`${newState.id}`);
+        if (!user) return;
+
+        const embed = new EmbedBuilder()
+            .setColor(color)
+            .setDescription(`**${user} switched from <#${oldState.channelId}> to <#${newState.channelId}>**`)
+            .setTimestamp()
+
+        if (user.user.avatar) embed.setFooter({ text: `User: ${user.user.tag}`, iconURL: `${user.user.avatarURL({ dynamic: true })}` })
+
+        logChannel.send({
+            embeds: [embed]
+        }).catch((e => { }));
+
+    }
+}
