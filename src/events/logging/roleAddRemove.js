ea1d46be08fae815c6c19d732108cbb62902be6e
Joa_sss <quabotgithub@gmail.com>
2022-08-24 20:17:57 +0200

    feat: added role add remove logging

---
+++ src/events/logging/roleAddRemove.js
@@ -0,0 +1,50 @@
+const { Client, EmbedBuilder, Colors, GuildBan, GuildMember } = require('discord.js');
+const { getLogConfig, getLogChannel } = require('../../structures/functions/config');
+
+module.exports = {
+    event: "guildMemberUpdate",
+    name: "roleAddRemove",
+    /**
+     * @param {GuildMember} oldMember
+     * @param {GuildMember} newMember
+     * @param {Client} client
+     */
+    async execute(oldMember, newMember, client, color) {
+
+        if (!oldMember.guild) return;
+
+        const logConfig = await getLogConfig(client, oldMember.guild.id);
+        if (!logConfig) return;
+
+        const logChannel = await getLogChannel(oldMember.guild, logConfig);
+        if (!logChannel) return;
+
+        if (!logConfig.enabledEvents.includes(this.name)) return;
+
+        if (oldMember.nickname !== newMember.nickname) return;
+        if (oldMember.communicationDisabledUntilTimestamp !== newMember.communicationDisabledUntilTimestamp) return;
+        if (oldMember.premiumSinceTimestamp !== newMember.premiumSinceTimestamp) return;
+        if (oldMember.avatar !== newMember.avatar) return;
+
+        let word;
+        if (oldMember._roles.length > newMember._roles.length) word = "Removed";
+        if (oldMember._roles.length < newMember._roles.length) word = "Given";
+
+        let role;
+        if (oldMember._roles < newMember._roles) role = `<@&${newMember._roles.filter(n => !oldMember._roles.includes(n)).join('>\n<@&')}>`;
+        if (oldMember._roles > newMember._roles) role = `<@&${oldMember._roles.filter(n => !newMember._roles.includes(n)).join('>\n<@&')}>`;
+
+        if (role === "<@&>") return;
+
+        const embed = new EmbedBuilder()
+            .setDescription(`**Roles ${word}**\n${role}`)
+            .setColor(Colors.Yellow)
+            .setTimestamp()
+
+        if (newMember.user.avatar) embed.setFooter({ text: `User: ${newMember.user.tag}`, iconURL: `${newMember.user.avatarURL({ dynamic: true })}` })
+
+        logChannel.send({
+            embeds: [embed]
+        }).catch((e => { }));
+    }
+}
