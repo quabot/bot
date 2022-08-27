158960ce746747fa9b9584641166bf7e16485abd
Joa_sss <quabotgithub@gmail.com>
2022-08-27 12:14:47 +0200

    feat: remade /emotes

--- src/commands/commands/misc/emotes.js
+++ src/commands/commands/misc/emotes.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+const { ApplicationCommandOptionType, EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder, Colors } = require('discord.js');
 
 module.exports = {
     name: "emotes",
@@ -10,15 +10,74 @@ module.exports = {
 
         await interaction.deferReply();
 
-        const emoteList = interaction.guild.emojis.cache.map(e => ` ${e}`);
+        const emoteList = [];
+        interaction.guild.emojis.cache.forEach(e => {
+            emoteList.push(e);
+        });
 
-        interaction.editReply({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`${emoteList}`)
-                    .setTimestamp()
-            ]
-        }).catch((e => { }));
+        const backId = 'backMusic'
+        const forwardId = 'forwardMusic'
+        const backButton = new ButtonBuilder({
+            style: ButtonStyle.Secondary,
+            label: 'Back',
+            emoji: '⬅️',
+            customId: backId
+        });
+        const forwardButton = new ButtonBuilder({
+            style: ButtonStyle.Secondary,
+            label: 'Forward',
+            emoji: '➡️',
+            customId: forwardId
+        });
+
+        const makeEmbed = async start => {
+            const current = emoteList.slice(start, start + 10);
+
+            return new EmbedBuilder({
+                color: Colors.Green,
+                timestamp: Date.now(),
+                title: `Emotes ${start + 1}-${start + 10}/${emoteList.length}`,
+                fields: await Promise.all(
+                    current.map(async (emote) => {
+                        return ({
+                            name: `${emote.name}`,
+                            value: `${emote}`,
+                            inline: true
+                        });
+                    })
+                )
+            });
+        }
+
+        let currentIndex = 0;
+
+        const canFit = emoteList.length <= 10;
+        const msg = await interaction.editReply({
+            embeds: [await makeEmbed(0)],
+            ephemeral: true,
+            components: canFit
+                ? []
+                : [new ActionRowBuilder({ components: [forwardButton] })]
+        })
+        if (canFit) return;
+
+        const collector = msg.createMessageComponentCollector({ filter: ({ user }) => user.id === user.id });
+
+        collector.on('collect', async interaction => {
+
+            interaction.customId === backId ? (currentIndex -= 10) : (currentIndex += 10)
+            await interaction.update({
+                embeds: [await makeEmbed(currentIndex)],
+                components: [
+                    new ActionRowBuilder({
+                        components: [
+                            ...(currentIndex ? [backButton] : []),
+                            ...(currentIndex + 10 < emoteList.length ? [forwardButton] : []),
+                        ]
+                    })
+                ]
+            });
+        });
+
     }
 }
