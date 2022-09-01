3ae5141a5d947320d5f477a1f929df6ff4f5e69c
Joa_sss <quabotgithub@gmail.com>
2022-09-01 21:25:04 +0200

    fix: (possibly) fixed a crash with messagedelete.

--- src/events/logging/messageDelete.js
+++ src/events/logging/messageDelete.js
@@ -9,41 +9,44 @@ module.exports = {
      * @param {Client} client
      */
     async execute(message, client, color) {
+        try {
 
-        if (!message.guildId) return;
+            if (!message.guildId) return;
 
-        const logConfig = await getLogConfig(client, message.guildId);
-        if (!logConfig) return;
+            const logConfig = await getLogConfig(client, message.guildId);
+            if (!logConfig) return;
 
-        const logChannel = await getLogChannel(message.guild, logConfig);
-        if (!logChannel) return;
+            const logChannel = await getLogChannel(message.guild, logConfig);
+            if (!logChannel) return;
 
-        if (!logConfig.enabledEvents.includes(this.event)) return;
+            if (!logConfig.enabledEvents.includes(this.event)) return;
 
-        let description = "**Message Deleted**";
+            let description = "**Message Deleted**";
 
-        let content = String(message.content);
-        if (content === null) return;
-        if (content.length === 0) return;
-        if (content.length > 1003) content = content.slice(0, 1002);
+            let content = String(message.content);
+            if (content === null) return;
+            if (content.length === 0) return;
+            if (content.length > 1003) content = content.slice(0, 1002);
 
-        description = `${description}\n${content}`;
+            description = `${description}\n${content}`;
 
-        const embed = new EmbedBuilder()
-            .setDescription(`${description}`)
-            .setColor(Colors.Red);
-        if (message.author.avatar) embed.setFooter({ text: `User: ${message.author.tag}`, iconURL: `${message.author.avatarURL({ dynamic: true })}` })
+            const embed = new EmbedBuilder()
+                .setDescription(`${description}`)
+                .setColor(Colors.Red);
+            if (message.author.avatar) embed.setFooter({ text: `User: ${message.author.tag}`, iconURL: `${message.author.avatarURL({ dynamic: true })}` })
 
-        embed.addFields({ name: "Channel", value: `${message.channel}`, inline: true });
-        if (message.attachments !== null) {
-            message.attachments.map(getUrls);
-            function getUrls(item) {
-                embed.addFields({ name: `**Attachment:**`, value: `${[item.url].join(" ")}` })
+            embed.addFields({ name: "Channel", value: `${message.channel}`, inline: true });
+            if (message.attachments !== null) {
+                message.attachments.map(getUrls);
+                function getUrls(item) {
+                    embed.addFields({ name: `**Attachment:**`, value: `${[item.url].join(" ")}` })
+                }
             }
-        }
 
-        logChannel.send({
-            embeds: [embed]
-        }).catch((e => { }));
+            logChannel.send({
+                embeds: [embed]
+            }).catch((e => { }));
+
+        } catch (e) { }
     }
 }
