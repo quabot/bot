e4582087bedbefb0aaa70b442d59f7341f208243
Joa_sss <quabotgithub@gmail.com>
2022-09-06 01:13:31 +0200

    feat: added guildreate event (db creation + msg)

---
+++ src/events/systems/botAdd.js
@@ -0,0 +1,36 @@
+const { getApplicationConfig, getCustomizationConfig, getGiveawayConfig, getLevelConfig, getLogConfig, getMembersConfig, getModerationConfig, getPollConfig, getRolesConfig, getTicketConfig, getVerifyConfig, getWelcomeConfig } = require('../../structures/functions/config')
+const { checkChannel } = require('../../structures/functions/channel');
+const { ChannelType, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    event: "guildCreate",
+    name: "botAdd",
+    async execute(guild, client, color) {
+        await getApplicationConfig(client, guild.id);
+        await getCustomizationConfig(client, guild.id);
+        await getGiveawayConfig(client, guild.id);
+        await getLevelConfig(client, guild.id);
+        await getLogConfig(client, guild.id);
+        await getMembersConfig(client, guild.id);
+        await getModerationConfig(client, guild.id);
+        await getPollConfig(client, guild.id);
+        await getRolesConfig(client, guild.id);
+        await getTicketConfig(client, guild.id);
+        await getVerifyConfig(client, guild.id);
+        await getWelcomeConfig(client, guild.id);
+
+        var channel = guild.channels.cache.filter(ch => ch.type === ChannelType.GuildText).find(x => x.position === 0);
+
+        if (!checkChannel(channel.type)) return;
+
+        channel.send(({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("Hello, I'm QuaBot.")
+                    .setTimestamp()
+                    .setDescription("I'm QuaBot, a multipurpose bot with loads of features. To configure me, go to [my dashboard](https://dashboard.quabot.net). If you need help with anything, join [my support server](https://discord.quabot.net).\nThanks for adding me to your server!")
+                    .setColor(color)
+            ]
+        })).catch((err => { }));
+    }
+}
