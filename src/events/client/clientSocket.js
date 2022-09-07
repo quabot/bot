585c72488270598238b2de3bb695f60be889ebc2
Joa_sss <quabotgithub@gmail.com>
2022-09-07 21:37:35 +0200

    feat: added socket

---
+++ src/events/client/clientSocket.js
@@ -0,0 +1,34 @@
+const { Client } = require('discord.js');
+const consola = require('consola');
+require('dotenv').config();
+
+module.exports = {
+    event: "ready",
+    name: "clientConnections",
+    once: true,
+    /**
+     * @param {Client} client
+     */
+    async execute(client) {
+
+        const { io } = require("socket.io-client");
+        const socket = io("http://localhost:3002");
+        socket.on("connect", () => {
+            consola.info("Socket connected.");
+
+            const engine = socket.io.engine;
+            engine.on("packet", ({ type, data }) => {
+                if (type !== 'message') return;
+                const index = data.indexOf(",");
+                const sliced = data.slice(index, data.length);
+                const cache = sliced.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/['"]+/g, '');
+
+                client.cache.take(cache);
+            });
+        });
+
+        socket.on("disconnect", () => {
+            consola.info("Socket disconnected.");
+        });
+    }
+}
