272d20fcc48dab27ad3a874cf3a487d5bb121839
Joa_sss <quabotgithub@gmail.com>
2022-09-25 19:18:11 +0200

    feat: /reaction delete now removes bot reaction

--- src/commands/subcommands/management/reactionroles-delete.js
+++ src/commands/subcommands/management/reactionroles-delete.js
@@ -1,4 +1,4 @@
-const { Client, ChannelType, EmbedBuilder } = require("discord.js");
+const { Client, ChannelType, EmbedBuilder, Message } = require("discord.js");
 const { generateEmbed } = require("../../../structures/functions/embed");
 const Reaction = require('../../../structures/schemas/ReactionRoleSchema');
 
@@ -14,11 +14,11 @@ module.exports = {
         await interaction.deferReply({ ephemeral: true }).catch((e => { }));
 
         const message_id = interaction.options.getString("message_id");
-        const role = interaction.options.getRole("role");
+        const channel = interaction.options.getChannel("channel");
         const emoji = interaction.options.getString("emoji");
 
-        if (!await Reaction.findOne({ guildId: interaction.guild.id, messageId: message_id, emoji: emoji, roleId: role.id })) return interaction.editReply({
-            ephemeral: true, embeds: [await generateEmbed(color, "That reaction role doesn't exist.")],
+        if (!await Reaction.findOne({ guildId: interaction.guild.id, messageId: message_id, emoji: emoji, channelId: channel.id })) return interaction.editReply({
+            embeds: [await generateEmbed(color, "That reaction role doesn't exist.")],
         }).catch((e => { }));
 
 
@@ -26,8 +26,22 @@ module.exports = {
             guildId: interaction.guild.id,
             messageId: message_id,
             emoji: emoji,
-            roleId: role.id,
+            channelId: channel.id,
         });
+
+        const message = await channel.messages.fetch({ message: message_id })
+            .catch(async e => {
+                await interaction.editReply({
+                    ephemeral: true, embeds: [
+                        new EmbedBuilder()
+                            .setColor(color)
+                            .setDescription("Could not find that message.")
+                    ]
+                }).catch((e => { }));
+                return;
+            });
+
+        message.reactions.resolve(emoji).users.remove(client.user.id).catch((e => { }));
 
         interaction.editReply({
             ephemeral: true, embeds: [await generateEmbed(color, "Successfully deleted the reaction role.")],
