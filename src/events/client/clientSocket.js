4a3b28b45e43eb186657678bf7cfa453fc14d677
Joa_sss <quabotgithub@gmail.com>
2022-09-26 14:08:54 +0200

    feat: added dashboard msg sending

--- src/events/client/clientSocket.js
+++ src/events/client/clientSocket.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js');
+const { Client, EmbedBuilder, Colors, ButtonStyle, ButtonBuilder, ActionRowBuilder } = require('discord.js');
 const consola = require('consola');
 require('dotenv').config();
 
@@ -20,6 +20,65 @@ module.exports = {
         socket.on("update", (data) => {
             client.cache.take(data.cache);
         });
+
+        socket.on("send", (data) => {
+            const channel = client.guilds.cache.get(data.guildId).channels.cache.get(data.channelId);
+            if (!channel) return;
+            if (data.type === "suggestion") {
+                channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Green)
+                            .setTitle("Create suggestion")
+                            .setDescription("Click on the button below this message to leave a suggestion.")
+                    ],
+                    components: [
+                        new ActionRowBuilder()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setCustomId("suggestion-create")
+                                    .setStyle(ButtonStyle.Secondary)
+                                    .setLabel("💡 Suggest")
+                            )
+                    ]
+                }).catch((e => { }));
+            } else if (data.type === "ticket") {
+                channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Green)
+                            .setTitle("Create ticket")
+                            .setDescription("Click on the button below this message to create a ticket.")
+                    ],
+                    components: [
+                        new ActionRowBuilder()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setCustomId("create-ticket")
+                                    .setStyle(ButtonStyle.Secondary)
+                                    .setLabel("🎫 Ticket")
+                            )
+                    ]
+                }).catch((e => { }));
+            } else {
+                channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Green)
+                            .setTitle("Please verify to get access to the server!")
+                            .setDescription("Click the button below this message to get verified.")
+                    ], components: [
+                        new ActionRowBuilder()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setCustomId("verify-server")
+                                    .setLabel("Verify")
+                                    .setStyle(ButtonStyle.Secondary)
+                            )
+                    ]
+                }).catch((e => { }));
+            }
+        })
 
         socket.on("disconnect", () => {
             consola.warn("Websocket disconnected.");
