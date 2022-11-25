735686c76ee5be944191382eb5d6a4ab2989e023
Joa_sss <quabotgithub@gmail.com>
2022-11-25 22:44:43 +0100

    feat: /support

--- src/commands/info/support.ts
+++ src/commands/info/support.ts
@@ -3,8 +3,8 @@ import { embed } from "../../utils/constants/embeds";
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('members')
-        .setDescription("Get the amount of people in the server.")
+        .setName('support')
+        .setDescription("Get an invite to our support server.")
         .setDMPermission(false),
     async execute(client: Client, interaction: CommandInteraction, color: any) {
 
@@ -14,8 +14,8 @@ module.exports = {
             embeds: [
                 embed(color)
                     .setThumbnail(`${interaction.guild?.iconURL()}`)
-                    .setTitle(`${interaction.guild?.name}`)
-                    .setDescription(`${interaction.guild?.memberCount}`)
+                    .setTitle(`QuaBot Support`)
+                    .setDescription(`Join our support server [here](https://discord.gg/kxKHuy47Eq) for fun, events, questions and suggestions!`)
             ]
         });
     }
