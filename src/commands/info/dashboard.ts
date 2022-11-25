3c222c1adae76f3699435a68fd365d5a0475e7d5
Joa_sss <quabotgithub@gmail.com>
2022-11-25 22:50:20 +0100

    feat: added /dashboard

---
+++ src/commands/info/dashboard.ts
@@ -0,0 +1,22 @@
+import { Client, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { embed } from "../../utils/constants/embeds";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dashboard')
+        .setDescription("Get the link of our dashboard.")
+        .setDMPermission(false),
+    async execute(client: Client, interaction: CommandInteraction, color: any) {
+
+        await interaction.deferReply();
+
+        await interaction.editReply({
+            embeds: [
+                embed(color)
+                    .setThumbnail(`${interaction.guild?.iconURL()}`)
+                    .setTitle(`QuaBot Dashboard`)
+                    .setDescription(`You can find our dashboard [here](https://quabot.net)!`)
+            ]
+        });
+    }
+}
