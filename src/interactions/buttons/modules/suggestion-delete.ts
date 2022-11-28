153ace9692bac5533ecd4c3323102ff7401fa2b0
Joa_sss <quabotgithub@gmail.com>
2022-11-28 09:25:38 +0100

    feat: added suggestion delete button

--- src/interactions/buttons/modules/suggestion-delete.ts
+++ src/interactions/buttons/modules/suggestion-delete.ts
@@ -1,5 +1,7 @@
-import { type Client, ButtonInteraction, ColorResolvable } from "discord.js";
+import { type Client, ButtonInteraction, ColorResolvable, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors } from "discord.js";
 import Suggest from '../../../structures/schemas/SuggestSchema';
+import { getSuggestConfig } from "../../../utils/configs/getSuggestConfig";
+import CustomEmbed from "../../../utils/constants/customEmbed";
 import Embed from "../../../utils/constants/embeds";
 
 module.exports = {
@@ -12,14 +14,104 @@ module.exports = {
             guildId: interaction.guildId,
             id: suggestionId
         });
-        
         if (!suggestion) return await interaction.editReply({
-            embeds:[
+            embeds: [
                 new Embed(color)
                     .setDescription('Couldn\'t find the suggestion.')
             ]
         });
 
-        // delete and edit log msg
+        const config: any = await getSuggestConfig(_client, interaction.guildId);
+        if (!config) return await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription('We just created a new document! Could you please run that command again?')
+            ]
+        });
+        if (!config.enabled) return await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription('Suggestions are disabled in this server!')
+            ]
+        });
+
+        const channel: any = interaction.guild.channels.cache.get(config.channelId);
+        if (!channel) return await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription('Couldn\'t find the suggestions channel.')
+            ]
+        });
+
+        await channel.messages
+            .fetch(suggestion.msgId)
+            .then(async (message: any) => {
+                if (!message)
+                    return interaction.editReply({
+                        embeds: [
+                            new Embed(color)
+                                .setDescription('Couldn\'t find the suggestion! Are you sure it wasn\'t deleted?')
+                        ]
+                    });
+
+                await message.delete();
+                await interaction.editReply({
+                    embeds: [
+                        new Embed(color)
+                            .setDescription('Suggestion deleted.')
+                    ]
+                });
+
+                await Suggest.findOneAndDelete({ id: suggestionId, guildId: interaction.guildId });
+
+                await interaction.message.edit({
+                    embeds: [
+                        new Embed(config.colors.deleted)
+                            .setTitle("New Suggestion")
+                            .addFields(
+                                { name: "User", value: `${interaction.message.embeds[0].fields[0].value}`, inline: true },
+                                { name: "State", value: `Deleted`, inline: true },
+                                { name: "ID", value: `${suggestion.id}`, inline: true },
+                                { name: "Suggestion", value: `${suggestion.suggestion}`, inline: false },
+                            )
+                    ], components: [
+                        new ActionRowBuilder<ButtonBuilder>()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setCustomId('suggestion-approve')
+                                    .setLabel('Approve')
+                                    .setDisabled(true)
+                                    .setStyle(ButtonStyle.Success),
+                                new ButtonBuilder()
+                                    .setDisabled(true)
+                                    .setCustomId('suggestion-deny')
+                                    .setLabel('Deny')
+                                    .setStyle(ButtonStyle.Danger),
+                                new ButtonBuilder()
+                                    .setDisabled(true)
+                                    .setCustomId('suggestion-delete')
+                                    .setLabel('Delete')
+                                    .setStyle(ButtonStyle.Secondary)
+                            )
+                    ]
+                })
+
+                if (!config.dm) return;
+
+                const user = interaction.guild.members.cache.get(`${suggestion.userId}`);
+                const parseString = (text: string) =>
+                    text
+                        .replaceAll('{suggestion}', suggestion.suggestion)
+                        .replaceAll('{user}', `${user}`)
+                        .replaceAll('{avatar}', user.displayAvatarURL() ?? '')
+                        .replaceAll('{server}', interaction.guild?.name ?? '')
+                        .replaceAll('{staff}', `${interaction.user ?? ''}`)
+                        .replaceAll('{state}', 'deleted')
+                        .replaceAll('{color}', ` ${config.colors.deleted}`)
+                        .replaceAll('{icon}', interaction.guild?.iconURL() ?? '');
+
+                const embed = new CustomEmbed(config.dmMessage, parseString);
+                user.send({ embeds: [embed], content: parseString(config.dmMessage.content) });
+            });
     },
 };
