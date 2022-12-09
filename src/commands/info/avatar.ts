a03795ad67038cbabd4dbd9d76fe2541e275a3b8
MaestroDagan <ditisdagan@hotmail.com>
2022-12-09 09:59:36 +0100

    avatar

--- src/commands/info/avatar.ts
+++ src/commands/info/avatar.ts
@@ -1,31 +1,15 @@
-import { ChatInputCommandInteraction, Client, SlashCommandBuilder } from 'discord.js';
-import Embed from '../../_utils/constants/embeds';
+import { Command } from '../../structures';
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('avatar')
-        .setDescription('Get an avatar')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('user')
-                .setDescription('Get the avatar of a user.')
-                .addUserOption(option =>
-                    option.setName('user').setDescription("The user you'd like to see the avatar from!")
-                )
-        )
-        .addSubcommand(subcommand => subcommand.setName('server').setDescription('Get the avatar of the server.'))
-        .setDMPermission(false),
-    async execute(_client: Client, interaction: ChatInputCommandInteraction, color: any) {
-        await interaction.deferReply();
-
-        const user = interaction.options.getUser('user') ?? interaction.user;
-
-        await interaction.editReply({
-            embeds: [
-                new Embed(color)
-                    .setImage(user.displayAvatarURL({ size: 1024, forceStatic: false }))
-                    .setTitle(`${user.tag}'s avatar`),
-            ],
-        });
-    },
-};
+export default new Command()
+    .setName('avatar')
+    .setDescription('Get an avatar')
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('user')
+            .setDescription('Get the avatar of a user.')
+            .addUserOption(option =>
+                option.setName('user').setDescription("The user you'd like to see the avatar from!")
+            )
+    )
+    .addSubcommand(subcommand => subcommand.setName('server').setDescription('Get the avatar of the server.'))
+    .setDMPermission(false);
