4ff1edd82b508f2ffc4c0511f7f3be73abbf6395
Joa_sss <quabotgithub@gmail.com>
2023-01-27 09:38:55 +0100

    Sticker Events

---
+++ src/events/logs/stickerCreate.js
@@ -0,0 +1,37 @@
+const { Client, Events, Sticker, Colors } = require('discord.js');
+const { getLoggingConfig } = require('../../utils/configs/loggingConfig');
+const { Embed } = require('../../utils/constants/embed');
+
+module.exports = {
+    event: Events.GuildStickerCreate,
+    name: "stickerCreate",
+    /**
+     * @param {Sticker} sticker
+     * @param {Client} client 
+     */
+    async execute(sticker, client) {
+
+        const config = await getLoggingConfig(client, sticker.guild.id);
+        if (!config) return;
+        if (!config.enabled) return;
+
+        if (!config.enabledEvents.includes('stickerCreate')) return;
+
+
+        const channel = sticker.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+
+
+        await channel.send({
+            embeds: [
+                new Embed(Colors.Green)
+                    .setDescription(`
+                        **Sticker Created**
+                        ${sticker.name} - [Full image](${sticker.url})
+                        ${sticker.description}
+                        `)
+                    .setFooter({ text: `${sticker.name}`, iconURL: `${sticker.url}` })
+            ]
+        });
+    }
+}
---
+++ src/events/logs/stickerDelete.js
@@ -0,0 +1,37 @@
+const { Client, Events, Sticker, Colors } = require('discord.js');
+const { getLoggingConfig } = require('../../utils/configs/loggingConfig');
+const { Embed } = require('../../utils/constants/embed');
+
+module.exports = {
+    event: Events.GuildStickerDelete,
+    name: "stickerDelete",
+    /**
+     * @param {Sticker} sticker
+     * @param {Client} client 
+     */
+    async execute(sticker, client) {
+
+        const config = await getLoggingConfig(client, sticker.guild.id);
+        if (!config) return;
+        if (!config.enabled) return;
+
+        if (!config.enabledEvents.includes('stickerDelete')) return;
+
+
+        const channel = sticker.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+
+
+        await channel.send({
+            embeds: [
+                new Embed(Colors.Red)
+                    .setDescription(`
+                        **Sticker Deleted**
+                        ${sticker.name} - [Full image](${sticker.url})
+                        ${sticker.description}
+                        `)
+                    .setFooter({ text: `${sticker.name}`, iconURL: `${sticker.url}` })
+            ]
+        });
+    }
+}
