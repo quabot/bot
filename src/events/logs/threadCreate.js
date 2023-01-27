c4295301782fd3ace9c128281c93bfef2b7ebcd3
Joa_sss <quabotgithub@gmail.com>
2023-01-27 09:52:04 +0100

    thread logging

---
+++ src/events/logs/threadCreate.js
@@ -0,0 +1,40 @@
+const { Client, Events, Colors, ThreadChannel } = require('discord.js');
+const { getLoggingConfig } = require('../../utils/configs/loggingConfig');
+const { Embed } = require('../../utils/constants/embed');
+const { channelTypesById } = require('../../utils/constants/discord');
+
+module.exports = {
+    event: Events.ThreadCreate,
+    name: "threadCreate",
+    /**
+     * @param {ThreadChannel} thread
+     * @param {boolean} newlyCreated
+     * @param {Client} client 
+     */
+    async execute(thread, newlyCreated, client) {
+
+        const config = await getLoggingConfig(client, thread.guildId);
+        if (!config) return;
+        if (!config.enabled) return;
+
+        if (!config.enabledEvents.includes('threadCreate')) return;
+        if (thread.parentId && (config.excludedCategories.includes(thread.parent.parentId) || config.excludedChannels.includes(thread.parentId))) return;
+
+
+        const channel = thread.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+
+
+        await channel.send({
+            embeds: [
+                new Embed(Colors.Green)
+                    .setDescription(`
+                        **${channelTypesById[thread.type]} Created**
+                        ${thread.name} - ${thread}
+                        **Created by:** <@${thread.ownerId}>
+                        
+                        `)
+            ]
+        });
+    }
+}
---
+++ src/events/logs/threadDelete.js
@@ -0,0 +1,40 @@
+const { Client, Events, Colors, ThreadChannel } = require('discord.js');
+const { getLoggingConfig } = require('../../utils/configs/loggingConfig');
+const { Embed } = require('../../utils/constants/embed');
+const { channelTypesById } = require('../../utils/constants/discord');
+
+module.exports = {
+    event: Events.ThreadDelete,
+    name: "threadDelete",
+    /**
+     * @param {ThreadChannel} thread
+     * @param {boolean} newlyCreated
+     * @param {Client} client 
+     */
+    async execute(thread, client) {
+console.log(thread)
+        const config = await getLoggingConfig(client, thread.guildId);
+        if (!config) return;
+        if (!config.enabled) return;
+
+        if (!config.enabledEvents.includes('threadDelete')) return;
+        if (thread.parentId && (config.excludedCategories.includes(thread.parent.parentId) || config.excludedChannels.includes(thread.parentId))) return;
+
+
+        const channel = thread.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+
+
+        await channel.send({
+            embeds: [
+                new Embed(Colors.Red)
+                    .setDescription(`
+                        **${channelTypesById[thread.type]} Deleted**
+                        ${thread.name} - Parent: ${thread.parent}
+                        **Created by:** <@${thread.ownerId}>
+                        
+                        `)
+            ]
+        });
+    }
+}
