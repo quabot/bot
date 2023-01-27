84b6bc5b8b3aeb698486d08126a9671baec3a62a
Joa_sss <quabotgithub@gmail.com>
2023-01-27 21:20:45 +0100

    Finished logging

---
+++ src/events/logs/messageDelete.js
@@ -0,0 +1,51 @@
+const { Client, Events, Colors, Message } = require('discord.js');
+const { getLoggingConfig } = require('../../utils/configs/loggingConfig');
+const { Embed } = require('../../utils/constants/embed');
+
+module.exports = {
+    event: Events.MessageDelete,
+    name: "messageDelete",
+    /**
+     * @param {Message} message
+     * @param {Client} client 
+     */
+    async execute(message, client) {
+
+        const config = await getLoggingConfig(client, message.guildId);
+        if (!config) return;
+        if (!config.enabled) return;
+
+        if (!config.enabledEvents.includes('messageDelete')) return;
+
+
+        const channel = message.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+
+        let description = '';
+        if (message.content) description += message.content.slice(0, 1002);
+
+
+        const embed = new Embed(Colors.Red)
+            .setDescription(`
+            **Message Deleted**
+            ${description}
+            `)
+            .addFields({ name: 'Channel', value: `${message.channel}`, inline: true })
+            .setFooter({
+                text: `User: ${message.author.tag}`,
+                iconURL: `${message.author.avatarURL({ dynamic: true })}`,
+            });
+
+
+        const attachments = [];
+        message.attachments.map(i => attachments.push(i.url));
+        if (attachments.length !== 0) embed.addFields(
+            { name: '**Attachments**', value: `${attachments.join('\n')}`.slice(0, 1024) }
+        );
+
+
+        await channel.send({
+            embeds: [embed]
+        });
+    }
+}
