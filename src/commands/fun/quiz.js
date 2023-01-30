e845c300b20a23d380d75932760dc3598f436cd0
Joa_sss <quabotgithub@gmail.com>
2023-01-30 10:35:10 +0100

    progress on quiz

--- src/commands/fun/quiz.js
+++ src/commands/fun/quiz.js
@@ -1,6 +1,7 @@
-const { SlashCommandBuilder, Client, CommandInteraction } = require("discord.js");
+const { SlashCommandBuilder, Client, CommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { Embed } = require("../../utils/constants/embed");
 const axios = require('axios');
+const { shuffleArray } = require("../../utils/functions/array");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,7 +14,7 @@ module.exports = {
      */
     async execute(client, interaction, color) {
         await interaction.deferReply();
-        
+
         const { data } = await axios.get('https://opentdb.com/api.php?amount=1&type=multiple');
         if (!data) return await interaction.editReply({
             embeds: [
@@ -21,9 +22,9 @@ module.exports = {
                     .setDescription('Failed to get a quiz question!')
             ]
         });
-        
 
-        const question = data.results;
+
+        const question = data.results[0];
         if (!question) return await interaction.editReply({
             embeds: [
                 new Embed(color)
@@ -31,12 +32,36 @@ module.exports = {
             ]
         });
 
+        const answersRaw = question.incorrect_answers;
+        answersRaw.push(question.correct_answer);
+        const answers = await shuffleArray(answersRaw);
 
-        console.log(question)
 
-        // get array
-        // shuffle
-        // create message
-        // handle the changes
+        const row = new ActionRowBuilder();
+        answers.forEach(answer => {
+            const button = new ButtonBuilder()
+                .setCustomId(`quiz-${answers.indexOf(answer)}`)
+                .setLabel(answer.replaceAll('&quot;', '"'))
+                .setStyle(ButtonStyle.Secondary);
+
+            row.addComponents(button);
+        });
+
+        const message = await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription(`${question.question}`.replaceAll('&quot;', '"'))
+            ], components: [row], fetchReply: true
+        });
+
+        const collector = message.createMessageComponentCollector({
+            filter: ({ user }) => user.id === interaction.user.id,
+            time: 60000
+        });
+
+        collector.on('collect', async interaction => {
+            if (interaction.customId === 'quiz-0') {
+            }
+        });
     }
 }
