8631bf9e4aced8f0772136ff24658cee81003665
Joa_sss <quabotgithub@gmail.com>
2023-06-04 21:51:07 +0200

    Fixed some logs bugs

--- src/events/logs/voiceMove.js
+++ src/events/logs/voiceMove.js
@@ -12,14 +12,17 @@ module.exports = {
      * @param {Client} client 
      */
     async execute(oldState, newState, client) {
-
+console.log(oldState, newState)
         if (oldState.member.user.bot || newState.member.user.bot) return;
         
         const config = await getLoggingConfig(client, oldState.guild.id);
         if (!config) return;
         if (!config.enabled) return;
 
-        if (!config.events.includes('voiceJoinLeave')) return;
+        if (oldState.streaming !== newState.streaming || oldState.suppress !== newState.suppress || oldState.selfVideo !== newState.selfVideo || oldState.selfMute !== newState.selfMute || oldState.selfDeaf !== newState.selfDeaf) return;
+        if (oldState.serverDeaf !== newState.serverDeaf || oldState.serverMute !== newState.serverMute) return;
+
+        if (!config.events.includes('voiceMove')) return;
         if (newState.channelId && (config.excludedCategories.includes(newState.channel.parentId) || config.excludedChannels.includes(newState.channelId))) return;
         if (oldState.channelId && (config.excludedCategories.includes(oldState.channel.parentId) || config.excludedChannels.includes(oldState.channelId))) return;
 
