e5e6e374b24178e04c2c7509382c5ee44980bd8a
Joa_sss <quabotgithub@gmail.com>
2023-06-07 21:41:01 +0200

    Changed to application schema

--- src/structures/schemas/Application.js
+++ src/structures/schemas/Application.js
@@ -20,14 +20,12 @@ const Application = new Schema({
     submissions_managers: reqArray, // People that can approve/deny
 
     ignored_roles: reqArray, //Roles that will not be allowed to use the form
+    allowed_roles: reqArray,
     reapply: reqBool, //Can users apply more than once?
     dashboard_allowed: reqBool, // Can users apply from the dashboard
     anonymous: reqBool, // Should usersnames be visible
     cooldown_enabled: reqBool, // Cooldown enabled?
-    cooldown: reqNum, // What is the cooldown
-
-    react_complete: reqBool, // Should a reaction be added when filled out
-    reaction: reqString, // What reaction should be added (if enabled)
+    cooldown: reqString, // What is the cooldown
 
     add_roles: reqArray, // What roles should be added when approved
     remove_roles: reqArray, // What roles should be removed when approved,
