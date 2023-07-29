40742fd5d3724c25964fab8e7764afa6daec51d2
Joa_sss <quabotgithub@gmail.com>
2023-07-29 20:34:02 +0200

    Finished application button

--- src/events/systems/applicationButton.js
+++ src/events/systems/applicationButton.js
@@ -1,4 +1,4 @@
-const { Client, Message, Events } = require('discord.js');
+const { Client, Message, Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { getUser } = require('../../utils/configs/user');
 const { Embed } = require('../../utils/constants/embed');
 const { getServerConfig } = require('../../utils/configs/serverConfig');
@@ -17,10 +17,50 @@ module.exports = {
         const id = interaction.customId;
 
         if (!id.startsWith('applications-fill-')) return;
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const serverConfig = await getServerConfig(client, interaction.guildId);
+        if (!serverConfig) return await interaction.editReply({
+            embeds: [
+                new Embed('#416683')
+                    .setDescription('We\'re still setting up some documents for first-time use. Please run the command again.')
+            ]
+        });
+        const color = serverConfig.color ?? '#416683';
+
+
         const appid = id.slice(18, id.length);
-        if (!appid) return interaction.reply({ content: 'The application form id couldn\'t be found.', ephemeral: true });
+        if (!appid) return await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription('The application form couldn\'t be found.')
+            ]
+        });
 
         const application = await Application.findOne({ id: appid, guildId: interaction.guildId });
-        if (!application) return interaction.reply({ content: 'The application form couldn\'t be found.', ephemeral: true });
+        if (!application) return await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription('The application form couldn\'t be found.')
+            ]
+        });
+
+
+        await interaction.editReply({
+            embeds: [
+                new Embed(color)
+                    .setDescription(`You can apply for **${application.name}** by clicking the button below.`)
+            ],
+            components: [
+                new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel('Apply')
+                            .setStyle(ButtonStyle.Link)
+                            .setURL(`https://quabot.net/dashboard/1007810461347086357/user/applications/form/${application.id}`)
+                    )
+            ]
+        });
     }
 }
