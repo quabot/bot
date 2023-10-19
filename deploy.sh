c32d49623ccc02f4825a80aa06c60bf9652b366f
MaestroDagan <ditisdagan@hotmail.com>
2023-10-19 15:20:08 +0200

    build(deploy.sh): :ambulance: really removed enters

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-echo 'Installing dependencies'
-yarn install
-echo 'Building'
-yarn build
-echo 'Pruning devDependencies'
-yarn install --production
-echo 'Deployed successfully!'
+echo 'Installing dependencies' yarn install echo 'Building' yarn build echo 'Pruning devDependencies' yarn install --production echo 'Deployed successfully!'
