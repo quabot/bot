d58578b711e76a3f83bfed14f4a57b5b8295d5d8
MaestroDagan <ditisdagan@hotmail.com>
2023-10-19 15:17:18 +0200

    build: :ambulance: removed enters

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-
-# echo 'Pulling code from GitHub'
-# git pull origin main
-
 echo 'Installing dependencies'
 yarn install
-
 echo 'Building'
 yarn build
-
 echo 'Pruning devDependencies'
 yarn install --production
-
 echo 'Deployed successfully!'
