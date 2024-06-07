f1def38003b313eb63b9e16d98ebf1b442fd8f1c
Joa_sss <quabotgithub@gmail.com>
2024-06-07 11:42:22 +0200

    fix same as above (attepmpt

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ npm install
 echo 'Building'
 npm run build
 echo 'Pruning devDependencies'
+copyfiles -u 1 src/**/*.ttf dist/
+echo 'Copying files'
 npm prune --omit=dev
 echo 'Deployed successfully!'
