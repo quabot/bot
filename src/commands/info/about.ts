e951ac97397667d6f715b342b1fd9e5fd4fc553a
Joa_sss <quabotgithub@gmail.com>
2024-07-24 15:55:03 +0200

    feat: new about overhaul

--- src/commands/info/about.ts
+++ src/commands/info/about.ts
@@ -37,11 +37,11 @@ export default {
       .setFooter({ text: 'Created by @joa_sss' })
       .setDescription(`Welcome to information center for **<:logo_quabot:1263823618123501608> [QuaBot](https://quabot.net)**! Here you can find loads of info about QuaBot and it's features. QuaBot uses slash commands, so the prefix to use it is \`/\`! We use interactions all throughout our commands and modules.
         
-            QuaBot was designed and developed by [Joa_sss](https://github.com/Joasss) and his [team](https://quabot.net/credits) and was written in Javascript with the [discord.js](https://discord.js.org) framework.
+            QuaBot was designed and developed by [Joa_sss](https://github.com/Joasss) and his team and was written in Typescript with the [discord.js](https://discord.js.org) framework.
         
             It's very easy to get started with QuaBot, by simply typing a \`/\`! If you're still stuck somewhere, you can join our **[Support Server](https://discord.gg/HYGA7Y6ptk)** and ask for help there. To view all commands use \`/help\`. Go to the next page for more information.
         
-            **[Website](https://quabot.net)** | **[Support](https://discord.gg/HYGA7Y6ptk)** | **[Invite](https://discord.com/oauth2/authorize?scope=bot%20applications.commands&client_id=995243562134409296)** | **[Dashboard](https://dashboard.quabot.net)**`);
+            **[Website](https://quabot.net)** | **[Support](https://discord.gg/HYGA7Y6ptk)** | **[Invite](https://discord.com/oauth2/authorize?scope=bot%20applications.commands&client_id=995243562134409296)** | **[Dashboard](https://quabot.net/login)**`);
 
     const embed2 = new Embed(color)
       .setAuthor({
@@ -56,8 +56,8 @@ export default {
             > - Command toggles: toggle and configure all of the commands that we offer.
             > - Module configuration: configure all of QuaBot's modules to the greatest extend.
             > - Server configuration: configure all of QuaBot's server settings.
-            > - User Dashboard: server members can view their own QuaBot profile.
-            All with responsive and intuitive UI and is very easy to use and understand.\n\nClick **[here](https://quabot.net/dashboard)** for our dashboard.`);
+            > - Send messages: send customized messages with our Message Sender, and edit them later.
+            All with responsive and intuitive UI and is very easy to use and understand.\n\nClick **[here](https://quabot.net/login)** for our dashboard.`);
 
     const embed3 = new Embed(color)
       .setAuthor({
@@ -70,10 +70,10 @@ export default {
             \n**Why should you vote?**
             It helps us a lot! Show us support without paying money, and it takes just a few seconds. You also get the following benefits:\n- You get listed in the QuaBot support server.\n- You get a 1.5x level xp boost for 12 hours.
             \n\n**How can i vote?**
-            - Go to the sites below.\n- Click the "Vote" button.\n- You will have to login.\n- It's as simple as that! Enjoy your perks. (Note: You only get benefits from Top.gg)
+            - Go to the sites below.\n- Click the "Vote" button.\n- You will have to login.\n- It's as simple as that! Enjoy your perks.
         
-            \n\n**Vote Links:**
-            🔗 **[Top.gg](https://top.gg/bot/995243562134409296)** | 🔗 **[Discordbotlist.com](https://discordbotlist.com/bots/quabot-2212)** | 🔗 **[Bots.gg](https://discord.bots.gg/bots/995243562134409296)**`);
+            \n\n**Vote Link:**
+            🔗 **[Top.gg](https://top.gg/bot/995243562134409296)**`);
 
     const embed4 = new Embed(color)
       .setAuthor({
@@ -82,15 +82,30 @@ export default {
       })
       .setThumbnail(`${clientUser.avatarURL({ forceStatic: false })}`)
       .setFooter({ text: 'Created by @joa_sss' }).setDescription(`
-            Our Terms of Service constitute a legally binding agreement made between you and QuaBot, concerning your access to and use of the bot. You agree that by utilizing the bot, you have read, understood, and agreed to be bound by all of our Terms of Service.
-            > By using our bot you agree to those terms. If you do not agree with our Terms of Service then you are expressly prohibited from using the bot and you must discontinue use immediately.
-            > Read our privacy policy, by using the bot, you agree to the collection and use of information in accordance with our Privacy Policy.
-            
-            **Where can i find these Terms of Service an Privacy Policy?**
-            You can find the Terms of Service **[here](https://quabot.net/tos)** and the Privacy Policy **[here](https://quabot.net/privacy)**`);
+                    Our Terms of Service constitute a legally binding agreement made between you and QuaBot, concerning your access to and use of the bot. You agree that by utilizing the bot, you have read, understood, and agreed to be bound by all of our Terms of Service.
+                    > By using our bot you agree to those terms. If you do not agree with our Terms of Service then you are expressly prohibited from using the bot and you must discontinue use immediately.
+                    > Read our privacy policy, by using the bot, you agree to the collection and use of information in accordance with our Privacy Policy.
+                    
+                    **Where can i find these Terms of Service an Privacy Policy?**
+                    You can find the Terms of Service **[here](https://quabot.net/tos)** and the Privacy Policy **[here](https://quabot.net/privacy)**`);
+
+
+    const embed5 = new Embed(color)
+      .setAuthor({
+        name: 'QuaBot | Credits',
+        iconURL: `${clientUser.avatarURL({ forceStatic: false })}`,
+      })
+      .setThumbnail(`${clientUser.avatarURL({ forceStatic: false })}`)
+      .setFooter({ text: 'Created by @joa_sss' }).setDescription(`
+                          QuaBot is complex project, and there are multiple people working hard to bring you the best experience. Here are the people that are current contributors to QuaBot:
+                          > - [Joas](https://github.com/Joasss) | CEO, Developer & Designer
+                          > - sjonsua | Manager & Designer
+                          > - Tim | Manager
+                          
+                          Thanks to everyone that is has worked on QuaBot. Without these people QuaBot wouldn't have been where it is today.`);
 
     //* Put the embeds in a list and get the page system setup.
-    const aboutEmbeds = [embed1, embed2, embed3, embed4];
+    const aboutEmbeds = [embed1, embed2, embed3, embed4, embed5];
 
     let page = 0;
 
@@ -146,7 +161,7 @@ export default {
             ],
             components: [disabledRow],
           })
-          .catch(() => {});
+          .catch(() => { });
       }
     });
   },
