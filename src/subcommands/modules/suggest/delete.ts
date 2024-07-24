6c3a8078f2b22499201bc48c7068d56406d12832
Joa_sss <quabotgithub@gmail.com>
2024-07-24 16:13:49 +0200

    fix: cmd no feedback suggestion dlete

--- src/subcommands/modules/suggest/delete.ts
+++ src/subcommands/modules/suggest/delete.ts
@@ -70,6 +70,10 @@ export default {
         embeds: [embed],
         content: parser.parse(config.dmMessage.content),
       });
+    }).catch(() => {
+      interaction.editReply({
+        embeds: [new Embed(color).setDescription("Couldn't find the suggestion! Are you sure it wasn't deleted?")],
+      });
     });
   },
 };
