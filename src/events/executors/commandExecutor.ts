43709b85c27c6c40ee220cf52db9a4d0d5d790b8
Joa_sss <quabotgithub@gmail.com>
2024-12-03 15:55:38 +0100

    Update commandExecutor.ts

--- src/events/executors/commandExecutor.ts
+++ src/events/executors/commandExecutor.ts
@@ -27,8 +27,7 @@ export default {
 
     //* If the user's userID is not in the passedUserIds array, add it to the array, but before that, send a message (with a return)
     if (!passedUserIds.includes(`${interaction.user.id}`)) {
-      passedUserIds.push(`${interaction.user.id}`);
-      return await interaction.reply({
+      await interaction.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(Colors.Red)
@@ -37,6 +36,7 @@ export default {
         ephemeral: false,
       });
     }
+    if (!passedUserIds.includes(`${interaction.user.id}`)) return passedUserIds.push(`${interaction.user.id}`);
 
     const config = await getServerConfig(client, interaction.guildId);
     if (config && config.disabledCommands && config.disabledCommands.includes(interaction.commandName))
